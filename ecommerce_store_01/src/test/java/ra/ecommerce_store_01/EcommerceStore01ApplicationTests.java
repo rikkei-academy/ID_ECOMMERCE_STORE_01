b3247da4d7bd2ca0abc9a4873744363f4a1440cd
unknown <chinxo12@gmail.com>
2023-03-08 14:53:36 +0700

    feature/#498/create_database_task

---
+++ ecommerce_store_01/src/test/java/ra/ecommerce_store_01/EcommerceStore01ApplicationTests.java
@@ -0,0 +1,13 @@
+package ra.ecommerce_store_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceStore01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
