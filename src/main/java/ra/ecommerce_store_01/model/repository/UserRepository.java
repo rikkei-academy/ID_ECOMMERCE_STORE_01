bc240c0a4278ad26a73c901a927fc33c71bf0561
unknown <you@example.com>
2023-03-08 17:59:02 -0800

    feature/#499/signin

---
+++ src/main/java/ra/ecommerce_store_01/model/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package ra.ecommerce_store_01.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ra.ecommerce_store_01.model.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Integer> {
+    User findByUserName(String userName);
+    boolean existsByUserName(String userName);
+    boolean existsByEmail(String email);
+}
