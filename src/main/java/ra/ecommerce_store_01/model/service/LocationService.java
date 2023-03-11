6af898c455a5e5fdf3974df18f91c31903a21469
ChuMinh28 <112063181+ChuMinh28@users.noreply.github.com>
2023-03-11 10:31:57 +0700

    feature/#703/product_location_task

--- src/main/java/ra/ecommerce_store_01/model/service/LocationService.java
+++ src/main/java/ra/ecommerce_store_01/model/service/LocationService.java
@@ -1,7 +1,4 @@
 package ra.ecommerce_store_01.model.service;
 
-import ra.ecommerce_store_01.model.entity.Location;
-
 public interface LocationService {
-    Location findById(int locationId);
 }
--- src/main/java/ra/ecommerce_store_01/model/serviveImp/LocationServiceImp.java
+++ src/main/java/ra/ecommerce_store_01/model/serviveImp/LocationServiceImp.java
@@ -1,17 +1,8 @@
 package ra.ecommerce_store_01.model.serviveImp;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import ra.ecommerce_store_01.model.entity.Location;
-import ra.ecommerce_store_01.model.repository.LocationRepository;
 import ra.ecommerce_store_01.model.service.LocationService;
 
 @Service
 public class LocationServiceImp implements LocationService {
-    @Autowired
-    LocationRepository locationRepository;
-    @Override
-    public Location findById(int locationId) {
-        return locationRepository.findById(locationId).get();
-    }
 }
