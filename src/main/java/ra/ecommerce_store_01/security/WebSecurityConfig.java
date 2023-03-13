46eba50b9c0e49eaec9238c4f160d694e6655296
trongtuyen0362086227 <tuyengiangxd1992@gmail.com>
2023-03-13 11:20:03 -0700

    feature/#705/ster_management_task

--- src/main/java/ra/ecommerce_store_01/security/WebSecurityConfig.java
+++ src/main/java/ra/ecommerce_store_01/security/WebSecurityConfig.java
@@ -61,6 +61,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/v1/product/**").permitAll()
                 .antMatchers("/api/v1/review/**").permitAll()
                 .antMatchers("/api/v1/auth/**").permitAll()// Cho phép tất cả mọi người truy cập vào địa chỉ này
+                .antMatchers("/api/v1/star/**").permitAll()
                 .anyRequest().authenticated(); // Tất cả các request khác đều cần phải xác thực mới được truy cập
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
